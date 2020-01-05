04cfd00668c0f13d664855da67fb2943ebcadad5
Udot Andrii <udot.dron@gmail.com>
2020-01-05 22:27:44 +0200

    projectReactByUdot050120

---
+++ precache-manifest.72de68298000dc3145dc2d32f716562f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c11cbc9f4d002b94ed29feb24a12bba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a83d17b3aebc0360c5b",
+    "url": "/static/css/2.8d392223.chunk.css"
+  },
+  {
+    "revision": "ec70fececf74a8f6c605",
+    "url": "/static/css/main.09d75673.chunk.css"
+  },
+  {
+    "revision": "3a83d17b3aebc0360c5b",
+    "url": "/static/js/2.356a265c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.356a265c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec70fececf74a8f6c605",
+    "url": "/static/js/main.8ed86978.chunk.js"
+  },
+  {
+    "revision": "11f9b9bea2e2ee2f8248",
+    "url": "/static/js/runtime-main.c20a87d2.js"
+  },
+  {
+    "revision": "8c35d0d213bc5c2f20c59f7cfc1a7e1d",
+    "url": "/static/media/404.8c35d0d2.png"
+  },
+  {
+    "revision": "920c39f0d0cfbdb219de669414e16875",
+    "url": "/static/media/about.920c39f0.jpg"
+  },
+  {
+    "revision": "39a899f044cd721ca5dd11669e6c2560",
+    "url": "/static/media/banner_575x246-575x246.39a899f0.jpg"
+  },
+  {
+    "revision": "9fc28f468d73acdf9947832818636e46",
+    "url": "/static/media/del_item.9fc28f46.png"
+  },
+  {
+    "revision": "2af97ed5d63f9beb3ff2ab0b6df0a9e5",
+    "url": "/static/media/logo.2af97ed5.jpg"
+  },
+  {
+    "revision": "8dc2c2814ac2ddbe6e35199ca284bb3a",
+    "url": "/static/media/noimage.8dc2c281.png"
+  }
+]);
